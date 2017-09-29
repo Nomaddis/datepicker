2162787c16c94f0ba84cd90a72fb9c43f0030b86
Serhiy <nomaddis@gmail.com>
2017-09-29 15:27:54 +0300

    'initial_commit'

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var pug = require('gulp-pug'),
+    gulp = require('gulp'),
+    less = require('gulp-less');
+    browserSync = require("browser-sync"),
+    path = require('path'),
+    reload = browserSync.reload;
+var config = {
+    server: {
+        baseDir: "./web"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Devil"
+};
+gulp.task('less', function() {
+    return gulp.src('less/all.less') // Gets all files ending with .styl in stylys/styl and children dirs
+        .pipe(less())
+        .pipe(gulp.dest('web/css/'));
+});
+gulp.task('pug', function buildHTML() {
+    return gulp.src('pug/**/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('web/')); // указываем gulp куда положить скомпилированные HTML файлы
+});
+gulp.task('browser-sync', function () {
+    browserSync(config);
+});
+gulp.task('watch', ['browser-sync'], function(){
+    gulp.watch('less/**/*.less', ['less', browserSync.reload]);
+    gulp.watch('pug/**/*.pug', ['pug', browserSync.reload]);
+    // Other watchers
+})
+
+gulp.task('default', ['browser-sync', 'watch']);
